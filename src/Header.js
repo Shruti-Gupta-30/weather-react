0916ab1a6811eb65368daad7a9834e2251f4e536
Shruti Gupta <guptashruti.0310@gmail.com>
2023-08-25 06:31:24 +0530

    Made layout for forecast

--- src/Header.js
+++ src/Header.js
@@ -36,9 +36,9 @@ export default function Header() {
 					xmlns="http://www.w3.org/2000/svg"
 					width="16"
 					height="16"
-					fill="currentColor"
+					fill="#4390c6"
 					class="bi bi-caret-left-fill"
-					viewBox="0 0 16 16"
+					viewBox="0 -0.5 17 17"
 				>
 					<path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
 				</svg>
@@ -65,9 +65,9 @@ export default function Header() {
 					xmlns="http://www.w3.org/2000/svg"
 					width="16"
 					height="16"
-					fill="currentColor"
+					fill="#4390c6"
 					class="bi bi-caret-right-fill"
-					viewBox="0 0 16 16"
+					viewBox="0 -0.5 15 17"
 				>
 					<path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
 				</svg>
